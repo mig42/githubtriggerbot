2389080cc613ebde8d2753c2c21de02961157e32
Miguel González <migueljgonz@gmail.com>
2017-11-19 18:41:00 +0100

    I didn't realise I needed the default unauthorised page!

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -42,6 +42,17 @@ namespace githubtriggerbot.Controllers
 
         [HttpGet]
         [AllowAnonymous]
+        public async Task<IActionResult> Login(string returnUrl = null)
+        {
+            // Clear the existing external cookie to ensure a clean login process
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
         public IActionResult Lockout()
         {
             return View();
